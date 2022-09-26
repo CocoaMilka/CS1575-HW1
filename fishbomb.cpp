f563c3cf4ddb765a2589dc78dfad7e7e10adb2e0
CocoaMilka <chocolatemilkcows@gmail.com>
2022-09-25 20:24:03 -0500

    Added initial file

---
+++ fishbomb.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+class Matrix
+{
+public:
+	int rows;
+	int columns;
+	int *data[];
+
+	Matrix(int r, int c)
+	{
+		rows = r;
+		columns = c;
+		*data = new int(r);
+	}
+
+	void populateMatrix()
+	{
+		int temp;
+		for (int i = 0; i < rows; i++)
+		{
+			data[i] = new int(columns);
+			for (int j = 0; j < columns; j++)
+			{
+				cin >> temp;
+				data[i][j] = temp;
+			}
+		}
+	}
+
+	void printMatrix()
+	{
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < columns; j++)
+			{
+				cout << data[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+};
+
+int main()
+{
+	//Matrix matrix1(3, 3);
+	//matrix1.populateMatrix();
+
+	cout << "enter input: ";
+	int test;
+	cin >> test;
+	cout << "done";
+}
